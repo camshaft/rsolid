bb05ebb6e957452fbaf99ae175f115a8e0a3cb4c
Cameron Bytheway <bytheway.cameron@gmail.com>
2024-06-27 18:05:29 -0600

    add import/export helpers

---
+++ rsolid/src/import.rs
@@ -0,0 +1,37 @@
+use super::*;
+use core::fmt;
+use scad::Scad;
+
+#[derive(Debug)]
+struct Import<const D: usize>(String);
+
+impl<const D: usize> scad::Scad for Import<D> {
+    #[inline]
+    fn assign(&self, f: &mut scad::Formatter) -> scad::Assignment {
+        f.emit(format!("import({:?})", self.0), scad::AssignmentType::Call)
+    }
+}
+
+impl<const D: usize> fmt::Display for Import<D> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(&self.to_scad())
+    }
+}
+
+impl<const D: usize> IntoObject<D> for Import<D> {
+    fn into_object(self) -> Object<D> {
+        Object::new(self)
+    }
+}
+
+macro_rules! import {
+    ($name:ident, $dim:literal) => {
+        pub fn $name<P: core::fmt::Display>(path: P) -> Object<$dim> {
+            Import(path.to_string()).into_object()
+        }
+    };
+}
+
+import!(svg, 2);
+import!(stl, 3);
+import!(amf, 3);
--- rsolid/src/lib.rs
+++ rsolid/src/lib.rs
@@ -6,12 +6,13 @@ mod block;
 mod ext;
 mod extension;
 mod helpers;
+pub mod import;
 pub mod mask;
 mod object;
 mod operator;
 mod parameter;
 mod primitive;
-mod scad;
+pub mod scad;
 mod shape;
 mod types;
 // TODO
@@ -20,8 +21,64 @@ mod types;
 pub use ext::*;
 pub use extension::*;
 pub use helpers::*;
+pub use import::*;
 pub use object::{IntoObject, Object};
 pub use operator::Operator;
 pub use primitive::*;
 pub use shape::*;
 pub use types::*;
+
+pub fn export<V: scad::Scad>(v: &V, crate_name: &str, renders: &[&str]) {
+    let out = v.to_scad();
+
+    let dir = std::path::Path::new("target/rsolid");
+    std::fs::create_dir_all(dir).unwrap();
+
+    let stem = dir.join(crate_name);
+    let scad = stem.with_extension("scad");
+    eprintln!("rendering {}...", scad.display());
+    std::fs::write(&scad, out).unwrap();
+    eprintln!("  done");
+
+    for ext in renders {
+        let out = stem.with_extension(ext);
+        eprintln!("rendering {}...", out.display());
+
+        let cmd = std::process::Command::new("openscad")
+            .arg("-o")
+            .arg("-")
+            .arg("--export-format")
+            .arg(ext)
+            .arg("--render")
+            .arg("true")
+            .arg(&scad)
+            .stderr(std::process::Stdio::inherit())
+            .stdout(std::process::Stdio::piped())
+            .spawn()
+            .unwrap()
+            .wait_with_output()
+            .unwrap();
+
+        assert!(cmd.status.success());
+
+        std::fs::write(out, cmd.stdout).unwrap();
+
+        println!("  done");
+    }
+}
+
+#[macro_export]
+macro_rules! export {
+    ($value:expr) => {
+        $crate::export!($value, &[]);
+    };
+    ($value:expr, $extra:expr) => {{
+        let name = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .file_stem()
+            .unwrap()
+            .to_str()
+            .unwrap();
+
+        $crate::export(&$value, name, $extra);
+    }};
+}
